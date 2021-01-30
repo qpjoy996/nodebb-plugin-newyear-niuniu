d83b2ed3f8b56d2f9f382909353ba6aadbaccc14
qpjoy <qpjoy2018@163.com>
2021-01-30 21:20:13 +0800

    init newyear style

---
+++ library.js
@@ -0,0 +1,6 @@
+var plugin = {},
+    user = require.main.require("./src/user"),
+    SocketPlugins = require.main.require("./src/socket.io/plugins");
+const Sockets = require.main.require("./src/socket.io/");
+
+module.exports = plugin;
---
+++ static/lib/main.js
@@ -0,0 +1,20 @@
+$(function () {
+  var fuzi =
+    '<img src="/forum/plugins/nodebb-plugin-newyear-niuniu/static/images/福字.gif" alt="f" class="fuziImg">';
+  var shizi =
+    '<img src="/forum/plugins/nodebb-plugin-newyear-niuniu/static/images/狮子.gif" alt="s" class="shiziImg"></img>';
+  $(document.body).append(fuzi, shizi);
+  $(".fuziImg").css({
+    width: "250px",
+    position: "absolute",
+    top: "-33px",
+    left: "30px",
+    zIndex: "9999"
+  });
+  $(".shiziImg").css({
+    width: "250px",
+    position: "absolute",
+    bottom: "0",
+    right: "30px",
+  });
+});
